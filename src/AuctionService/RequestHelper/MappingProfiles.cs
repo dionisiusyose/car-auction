22e50d01c8ed22754742ecc0e3c3a4d814144897
immortalsaurus <dionisiusyose11@gmail.com>
2026-01-31 20:43:18 +0700

    End of section 2

---
+++ src/AuctionService/RequestHelper/MappingProfiles.cs
@@ -0,0 +1,16 @@
+using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+
+namespace AuctionService.RequestHelper;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Auction, AuctionDTO>().IncludeMembers(x=>x.Item);
+        CreateMap<Item, AuctionDTO>();
+        CreateMap<CreateAuctionDTO, Auction>().ForMember(d=>d.Item, o=> o.MapFrom(s=>s));
+        CreateMap<CreateAuctionDTO, Item>();
+    }
+}

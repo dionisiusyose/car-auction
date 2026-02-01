facbea79762cf56728b0c76f5e868843bd2b6c13
immortalsaurus <dionisiusyose11@gmail.com>
2026-02-01 18:12:43 +0700

    End of section 4

--- src/AuctionService/Controllers/AuctionController.cs
+++ src/AuctionService/Controllers/AuctionController.cs
@@ -3,6 +3,8 @@ using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Contracts;
+using MassTransit;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,11 +16,13 @@ public class AuctionController : ControllerBase
 {
     private readonly AuctionDbContext _context;
     private readonly IMapper _mapper;
+    private readonly IPublishEndpoint _publishEndpoint;
 
-    public AuctionController(AuctionDbContext context, IMapper mapper)
+    public AuctionController(AuctionDbContext context, IMapper mapper, IPublishEndpoint publishEndpoint)
     {
         _context = context;
         _mapper = mapper;
+        _publishEndpoint = publishEndpoint;
     }
     [HttpGet]
     public async Task<ActionResult<List<AuctionDTO>>> GetAllAuctions(string date)
@@ -52,6 +56,10 @@ public class AuctionController : ControllerBase
 
         _context.Auctions.Add(auction);
         
+        var newAuction = _mapper.Map<AuctionDTO>(auction);
+
+        await _publishEndpoint.Publish(_mapper.Map<AuctionCreated>(newAuction));
+
         var result = await _context.SaveChangesAsync() > 0;
         
         if(!result) return BadRequest("Could not save changes to the DB");
@@ -75,6 +83,8 @@ public class AuctionController : ControllerBase
         auction.Item.Mileage = updateAuctionDTO.Mileage ?? auction.Item.Mileage;    
         auction.Item.Year = updateAuctionDTO.Year ?? auction.Item.Year;    
 
+        await _publishEndpoint.Publish(_mapper.Map<AuctionUpdated>(auction));
+
         var result = await _context.SaveChangesAsync() > 0;
 
         if (result) return Ok();
@@ -91,6 +101,8 @@ public class AuctionController : ControllerBase
         // TODO: Add current user as seller
 
         _context.Auctions.Remove(auction);
+
+        await _publishEndpoint.Publish<AuctionDeleted>(new {Id = auction.Id.ToString()});
 
         var result = await _context.SaveChangesAsync() > 0;
 
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,18 +1,38 @@
 using System.Net;
-using MongoDB.Driver;
-using MongoDB.Entities;
 using Polly;
 using Polly.Extensions.Http;
 using SearchService.Data;
-using SearchService.Models;
 using SearchService.Services;
+using MassTransit;
+using SearchService.Consumer;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+
+builder.Services.AddMassTransit(x=>
+{
+   x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+
+   x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
+
+   x.UsingRabbitMq((context, cfg) =>
+   {
+        cfg.ReceiveEndpoint("search-auction-created", e=>
+        {
+            e.UseMessageRetry(r=>r.Interval(5, 5));
+
+            e.ConfigureConsumer<AuctionCreatedConsumer>(context);
+        });
+
+       cfg.ConfigureEndpoints(context);
+   });
+});
+
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 var app = builder.Build();

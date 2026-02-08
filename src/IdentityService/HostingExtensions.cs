b8bbd68d41bf3b53c8cbb0cd570ee3ff0b0edc59
immortalsaurus <dionisiusyose11@gmail.com>
2026-02-08 17:53:47 +0700

    End of Section 7

--- src/IdentityService/HostingExtensions.cs
+++ src/IdentityService/HostingExtensions.cs
@@ -66,11 +66,10 @@ internal static class HostingExtensions
                 options.Events.RaiseFailureEvents = true;
                 options.Events.RaiseSuccessEvents = true;
 
-                // Use a large chunk size for diagnostic data in development where it will be redirected to a local file.
-                // if (builder.Environment.IsDevelopment())
-                // {
-                //     options.Diagnostics.ChunkSize = 1024 * 1024 * 10; // 10 MB
-                // }
+                if (builder.Environment.IsEnvironment("Docker"))
+                {
+                    options.IssuerUri = "http://localhost:5001";
+                }
             })
             .AddInMemoryIdentityResources(Config.IdentityResources)
             .AddInMemoryApiScopes(Config.ApiScopes)

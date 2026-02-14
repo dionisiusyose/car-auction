dcf17437dc7e5ac4567c9c253d3d9bfa0c937c90
immortalsaurus <dionisiusyose11@gmail.com>
2026-02-14 16:42:06 +0700

    End of Section 8

---
+++ frontend/web-app/app/nav/Search.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useParamsStore } from "@/hooks/useParamsStore";
+import { FaSearch } from "react-icons/fa";
+
+export default function Search() {
+  const searchTerm = useParamsStore((state) => state.searchTerm);
+  const setParams = useParamsStore((state) => state.setParams);
+
+  function handleSearch() {
+    // tidak perlu apa-apa karena sudah update realtime
+  }
+
+  return (
+    <div className="flex w-1/2 items-center border-2 border-gray-300 rounded-full px-4 py-3 shadow-sm">
+      <input
+        value={searchTerm ?? ""}
+        onChange={(e) => setParams({ searchTerm: e.target.value })}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") handleSearch();
+        }}
+        type="text"
+        placeholder="Search for cars by make, model or color"
+        className="
+            grow
+            pl-5
+            bg-transparent
+            outline-none
+            focus:ring-0
+            text-sm
+            text-gray-600
+            border-0
+            "
+      />
+
+      <button onClick={handleSearch}>
+        <FaSearch
+          size={34}
+          className="bg-red-400 text-white rounded-full p-2 cursor-pointer mx-2"
+        />
+      </button>
+    </div>
+  );
+}
---
+++ frontend/web-app/next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "cdn.pixabay.com" }],
+  },
+};
+
+export default nextConfig;
